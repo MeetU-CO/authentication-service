6d61f04c8ab54d53f96e0b7f6c9ee4be49a29c06
Richard Guevara <leonvilla6@outlook.com>
2022-11-13 13:26:49 -0500

    Users module and docker setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/infraestructure/api/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { urlConnection } from './user/infraestructure/implementation/mongodb/config';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(urlConnection), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
