9e4b6b2229959c4b42e647579e8eadf0828e2787
Richard Lion Guevara Puerta <81392855+SpentRook@users.noreply.github.com>
2022-11-14 18:40:42 -0500

    Feature/tests (#3) (#4)
    * Setup and initial structure with nestJS
    
    * Users module and docker setup
    
    * Create .gitattributes
    
    * Create wrong-password-email.exception.ts
    
    * Create user-not-found.exception.ts
    
    * Create http-code.entity.ts
    
    * Create already-user-exists.exception.ts
    
    * Create missing-env-variables.exception.ts
    
    * Create auth DTOs
    
    * Create user.repository.ts
    
    * Create notification.entity.ts
    
    * Create roles.entity.ts
    
    * Create user.entity.ts
    
    * Create create-user.service.ts
    
    * Create get-user.service.ts
    
    * Create signup-auth.service.ts
    
    * Create login-auth.service.ts
    
    * Create index.ts
    
    * Create user.model.ts
    
    * Create mongo-user.repository.ts
    
    * Create index.ts
    
    * Create bcrypjs.encrypter.ts
    
    * Create auth.controller.ts
    
    * Create user.service.ts
    
    * Update .gitattributes
    
    * Update .gitattributes
    
    * LF end line normalize
    
    * Update login-auth.service.ts
    
    * Introduce end-of-line normalization
    
    * Revert "Introduce end-of-line normalization"
    
    This reverts commit a7f5a98aadd4e8d75fece09029b0116597d9f25c.
    
    * LF normalize
    
    * LF normalize
    
    * LF normalize
    
    * LF normalize
    
    * Add unit test for login auth service
    
    * Delete unused classes
    
    * Fix notifications name in signup-auth.service
    
    * Update login-auth.service.spec.ts
    
    * Add singup-auth.service tests
    
    * Exclude false positives from coverage
    
    * Update signup-auth.service.spec.ts
    
    * Add create-user-service test
    
    * Add get-user.service test
    
    * Add user.controller test
    
    * Add user.service test
    
    * Add bycrpjs encrypter test
    
    * Add mongo user repo test

---
+++ src/user/application/service/auth/login-auth.service.spec.ts
@@ -0,0 +1,68 @@
+import { LoginAuthService } from './login-auth.service';
+import { GetUserService } from '../user/get-user.service';
+import { WrongPasswordOrEmailException } from 'src/user/domain/exception/wrong-password-email.exception';
+
+describe('LoginAuthService', () => {
+  const getUserServiceMock = jest
+    .spyOn(GetUserService.prototype, 'run')
+    .mockImplementation(async () => mockUserInDB);
+
+  const mockEncrypter = {
+    compare: jest.fn(async (password, hash) => password === hash),
+    encrypt: jest.fn(async (password) => password),
+  };
+
+  const mockUserInDB = {
+    email: 'user@example.com',
+    password: 'hashed',
+    name: 'user',
+    isVerified: false,
+    roles: [],
+    notifications: [],
+  };
+  const mockUserRepository = {
+    add: jest.fn(async (user) => user),
+    getByEmail: jest.fn(async (email) =>
+      email === mockUserInDB ? mockUserInDB : null,
+    ),
+  };
+
+  const loginAuthService: LoginAuthService = new LoginAuthService(
+    mockEncrypter,
+    mockUserRepository,
+  );
+
+  const mockAuthDTOSuccessful = {
+    email: 'user@example.com',
+    password: 'hashed',
+  };
+
+  const mockAuthDTOFailed = {
+    email: 'user@example.com',
+    password: 'other',
+  };
+
+  const authResponseDTOMock = {
+    email: 'user@example.com',
+    name: 'user',
+    roles: [],
+    notifications: [],
+  };
+
+  test('LoginAuthService execute GetUserService.run', async () => {
+    await loginAuthService.run(mockAuthDTOSuccessful);
+    expect(getUserServiceMock).toHaveBeenCalled();
+  });
+
+  test('LoginAuthService should return AuthResponseDTO when login is successful', async () => {
+    expect(await loginAuthService.run(mockAuthDTOSuccessful)).toStrictEqual(
+      authResponseDTOMock,
+    );
+  });
+
+  test('LoginAuthService should throw WrongPasswordOrEmailException when login is not successful', async () => {
+    await expect(loginAuthService.run(mockAuthDTOFailed)).rejects.toThrow(
+      WrongPasswordOrEmailException,
+    );
+  });
+});
