5d10a5e4309f1e51efd918cbcd0e20b4c4eb052d
Richard Guevara <leonvilla6@outlook.com>
2022-11-14 08:00:19 -0500

    Delete unused classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/infraestructure/api/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { urlConnection } from './user/infraestructure/implementation/mongodb/config';
 
 @Module({
   imports: [MongooseModule.forRoot(urlConnection), UserModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
