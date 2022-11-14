dc1cbd0d98a195df76b6204ae3f4687e65ebd279
Richard Guevara <leonvilla6@outlook.com>
2022-11-14 18:24:43 -0500

    Add get-user.service test

---
+++ src/user/application/service/user/get-user.service.spec.ts
@@ -0,0 +1,40 @@
+import { UserNotFoundException } from 'src/user/domain/exception/user-not-found.exception';
+import { GetUserService } from './get-user.service';
+
+describe('GetUserService', () => {
+  const mockUserRepository = {
+    add: jest.fn(async (user) => user),
+    getByEmail: jest.fn(async (email) =>
+      email === mockUserInDB.email ? mockUserInDB : null,
+    ),
+  };
+  const mockUserInDB = {
+    email: 'user@example.com',
+    password: 'hashed',
+    name: 'user',
+    isVerified: false,
+    roles: [],
+    notifications: [],
+  };
+
+  const emailFromNone = 'none@example.com';
+
+  const getUserService: GetUserService = new GetUserService(mockUserRepository);
+
+  test('GetUserService execute UserRepository.getByEmail', async () => {
+    await getUserService.run(mockUserInDB.email);
+    expect(mockUserRepository.getByEmail).toHaveBeenCalled();
+  });
+
+  test('GetUserService should return user when is found', async () => {
+    expect(await getUserService.run(mockUserInDB.email)).toStrictEqual(
+      mockUserInDB,
+    );
+  });
+
+  test('GetUserService should throw UserNotFoundException when is not found', async () => {
+    await expect(getUserService.run(emailFromNone)).rejects.toThrow(
+      UserNotFoundException,
+    );
+  });
+});
