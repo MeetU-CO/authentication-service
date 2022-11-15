91ff96906c3d5f471a1982d420df9ba8e5c24941
Richard Guevara <leonvilla6@outlook.com>
2022-11-14 22:53:54 -0500

    Add Helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
+import { AppModule } from './app.module';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+  app.use(cookieParser());
   app.use(csurf());
+  app.use(helmet());
+
   await app.listen(3001);
 }
 bootstrap();
