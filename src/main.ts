ae1d201d62fea32542aa6f81a1b04cf4ef2687b7
Richard Guevara <leonvilla6@outlook.com>
2022-11-14 23:22:16 -0500

    Remove csurf dependencia due to be deprecated

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
-import * as csurf from 'csurf';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.use(cookieParser());
-  app.use(csurf());
   app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
   await app.listen(3001);
 }
