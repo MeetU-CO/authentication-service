d6ebe6a926fb5983f29ec50050ab519de3885d52
Richard Lion Guevara Puerta <81392855+SpentRook@users.noreply.github.com>
2022-11-14 23:26:44 -0500

    Feature/security (#11)
    * Enable cors
    
    * Add CSRF Protection
    
    * Add Helmet middleware
    
    * Remove csurf dependencia due to be deprecated
    
    * Remove unused dtos
    
    * Add Validator for LoginAuthDTO
    
    * Add Validator for SignupAuthDTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(cookieParser());
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
   await app.listen(3001);
 }
 bootstrap();
