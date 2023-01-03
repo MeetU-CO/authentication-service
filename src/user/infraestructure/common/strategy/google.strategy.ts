033d25bc613e222ce0c22aebccf9d0020535f01f
Richard Guevara <leonvilla6@outlook.com>
2023-01-02 23:46:29 -0500

    Update google.strategy.ts

--- src/user/infraestructure/common/strategy/google.strategy.ts
+++ src/user/infraestructure/common/strategy/google.strategy.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { GoogleOauthConfigService } from '../../implementation/gcp/oauth2.0/config';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
-    super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: 'http://localhost:3001/auth/google/redirect',
-      scope: ['email', 'profile'],
-    });
+  constructor(googleConfig: GoogleOauthConfigService) {
+    super(googleConfig.getOauthOptions());
   }
 
   async validate(
