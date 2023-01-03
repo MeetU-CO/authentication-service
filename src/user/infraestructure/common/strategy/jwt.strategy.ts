881dcf712bd7b3c4423dffa280cd3b90143f133f
Richard Guevara <leonvilla6@outlook.com>
2023-01-02 23:45:08 -0500

    Update jwt.strategy.ts

--- src/user/infraestructure/common/strategy/jwt.strategy.ts
+++ src/user/infraestructure/common/strategy/jwt.strategy.ts
@@ -2,7 +2,6 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { JwtConfigService } from '../../implementation/jwt/config';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -10,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jwtConfigService.getJwtOptions(),
+      secretOrKey: jwtConfigService.getJwtOptions().secret,
     });
   }
 
