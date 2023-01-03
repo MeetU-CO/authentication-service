a8ef42db43964bbd7a6764f77f42f6b775a64dea
Richard Guevara <leonvilla6@outlook.com>
2023-01-03 03:02:30 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/infraestructure/api/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { urlConnection } from './user/infraestructure/implementation/mongodb/config';
+import { MongooseConfigService } from './user/infraestructure/implementation/mongodb/config';
+import { ConfigModule } from '@nestjs/config';
+import mongoEnvConfig from './user/infraestructure/implementation/mongodb/config/env.config';
+import jwtEnvConfig from './user/infraestructure/implementation/jwt/config/env.config';
+import oauthGoogleEnvConfig from './user/infraestructure/implementation/gcp/oauth2.0/config/env.config';
+import serverEnvConfig from './user/infraestructure/config/server/env.config';
 
 @Module({
-  imports: [MongooseModule.forRoot(urlConnection), UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        serverEnvConfig,
+        mongoEnvConfig,
+        jwtEnvConfig,
+        oauthGoogleEnvConfig,
+      ],
+      cache: true,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+    UserModule,
+  ],
 })
 export class AppModule {}
