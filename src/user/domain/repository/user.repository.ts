80c42273f3d5f3fe49b6b0006382d803faab4518
Richard Guevara <leonvilla6@outlook.com>
2023-01-11 16:27:02 -0500

    Update user.repository.ts

--- src/user/domain/repository/user.repository.ts
+++ src/user/domain/repository/user.repository.ts
@@ -3,4 +3,5 @@ import { User } from './entity/user.entity';
 export interface UserRepository {
   add: (user: User) => Promise<User>;
   getByEmail: (email: string) => Promise<User | null>;
+  deleteByEmail: (email: string) => Promise<boolean>;
 }
