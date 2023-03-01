5c5e734f29601d8afdfc2d43d110d21d175f6af4
Richard Guevara <leonvilla6@outlook.com>
2023-03-01 10:21:42 -0500

    Update env.config.ts

--- src/user/infraestructure/config/server/env.config.ts
+++ src/user/infraestructure/config/server/env.config.ts
@@ -3,6 +3,7 @@ import { registerAs } from '@nestjs/config';
 const serverEnvConfig = () => ({
   port: process.env.PORT ?? '3001',
   frontEndUrl: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+  grafanaLoggerApiKey: process.env.GRAFANA_LOGGING_API_KEY,
 });
 
 export default registerAs('server', serverEnvConfig);
