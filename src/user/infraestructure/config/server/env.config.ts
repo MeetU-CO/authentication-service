48b9dd0b95da17a2e1e0ae74d77e763138d8cf3e
Richard Guevara <leonvilla6@outlook.com>
2023-03-04 17:08:05 -0500

    Update env.config.ts

--- src/user/infraestructure/config/server/env.config.ts
+++ src/user/infraestructure/config/server/env.config.ts
@@ -1,9 +1,15 @@
 import { registerAs } from '@nestjs/config';
+import { MissingEnvVariablesException } from '../../../domain/exception/missing-env-variables.exception';
+import throwExpression from '../../../domain/utils/throw-expression';
 
 const serverEnvConfig = () => ({
   port: process.env.PORT ?? '3001',
   frontEndUrl: process.env.FRONTEND_URL ?? 'http://localhost:3000',
-  grafanaLoggerApiKey: process.env.GRAFANA_LOGGING_API_KEY,
+  grafanaLoggerApiKey:
+    process.env.GRAFANA_LOGGING_API_KEY ??
+    throwExpression(
+      new MissingEnvVariablesException('Grafana Logging API Key'),
+    ),
 });
 
 export default registerAs('server', serverEnvConfig);
