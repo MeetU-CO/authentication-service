0b14886e9e6c491bb58b27745fc082fcd94c6950
Richard Guevara <leonvilla6@outlook.com>
2023-03-04 17:07:47 -0500

    Create grafanaTransport.logger.ts

---
+++ src/user/infraestructure/implementation/logger/grafanaTransport.logger.ts
@@ -0,0 +1,18 @@
+import { TransportLogger } from '../../../domain/utils/logger';
+export class GrafanaTransport implements TransportLogger {
+  constructor(private apiKey: string) {}
+  createTranport() {
+    return {
+      target: 'pino-loki',
+      options: {
+        batching: true,
+        interval: 5,
+        host: 'https://logs-prod-017.grafana.net',
+        basicAuth: {
+          username: '369173',
+          password: this.apiKey,
+        },
+      },
+    };
+  }
+}
