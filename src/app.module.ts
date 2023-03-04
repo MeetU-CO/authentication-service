8638192d6605e55c9aed17f5d3390a7fc0735349
Richard Guevara <leonvilla6@outlook.com>
2023-03-04 17:08:13 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/infraestructure/api/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './user/infraestructure/implementation/mongodb/config';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import mongoEnvConfig from './user/infraestructure/implementation/mongodb/config/env.config';
 import jwtEnvConfig from './user/infraestructure/implementation/jwt/config/env.config';
 import oauthGoogleEnvConfig from './user/infraestructure/implementation/gcp/oauth2.0/config/env.config';
 import serverEnvConfig from './user/infraestructure/config/server/env.config';
 import oauthMicrosoftEnvConfig from './user/infraestructure/implementation/azure/oauth2.0/config/env.config';
+import { LoggerModule } from 'nestjs-pino';
+import { ConsoleTransport } from './user/infraestructure/implementation/logger/consoleTransport.logger';
+import { GrafanaTransport } from './user/infraestructure/implementation/logger/grafanaTransport.logger';
+import { TransportLogger } from './user/domain/utils/logger';
 
 @Module({
   imports: [
@@ -21,6 +25,26 @@ import oauthMicrosoftEnvConfig from './user/infraestructure/implementation/azure
         oauthMicrosoftEnvConfig,
       ],
       cache: true,
+    }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const transport: TransportLogger =
+          process.env.NODE_ENV === 'production'
+            ? new GrafanaTransport(
+                configService.get<string>('GRAFANA_LOGGING_API_KEY') as string,
+              )
+            : new ConsoleTransport();
+        return {
+          pinoHttp: {
+            customProps: (_req, _res) => ({
+              context: 'HTTP',
+            }),
+            transport: transport.createTranport(),
+          },
+        };
+      },
     }),
     MongooseModule.forRootAsync({
       useClass: MongooseConfigService,
