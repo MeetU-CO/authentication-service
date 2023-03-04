0d71309cbfd90c1f947f4bda8b8440f2abdd31e8
Richard Guevara <leonvilla6@outlook.com>
2023-03-04 17:08:15 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
   app.enableCors();
   app.use(cookieParser());
   app.use(helmet());
