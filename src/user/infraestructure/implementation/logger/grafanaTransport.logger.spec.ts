72553637060c0ebdd34c2542a49cc703256b09d7
Richard Guevara <leonvilla6@outlook.com>
2023-03-05 11:27:02 -0500

    Add grafanaTransport unit tests

---
+++ src/user/infraestructure/implementation/logger/grafanaTransport.logger.spec.ts
@@ -0,0 +1,21 @@
+import { GrafanaTransport } from "./grafanaTransport.logger";
+
+describe('grafanaTransport', () => {
+    const apiKey = "apiKey"
+    const grafanaTransport = new GrafanaTransport(apiKey);
+    const expectedTransport = {
+        target: 'pino-loki',
+        options: {
+          batching: true,
+          interval: 5,
+          host: 'https://logs-prod-017.grafana.net',
+          basicAuth: {
+            username: '369173',
+            password: apiKey,
+          },
+        },
+      };
+    it('createTransport should return the object transport', () => {
+      expect(grafanaTransport.createTransport()).toStrictEqual(expectedTransport);
+    });
+  });
